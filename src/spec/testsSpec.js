b6e09ec500c55342ac61d9a99a5ed8346fc33d54
Alexandre Fidélis Vieira Bitencourt <alexandrefvb@gmail.com>
2014-09-27 00:03:06 -0300

    Unit tests update

--- src/spec/testsSpec.js
+++ src/spec/testsSpec.js
@@ -1,6 +1,6 @@
 describe('Unit tests', function() {
 
-	it('check if simple attributes are serialized according to the ruless', function() {
+	it('check if simple attributes are serialized according to the rules', function() {
 		var json = $(
 			'<form>' +
 				'<input type="hidden" name="simpleInputHidden" value="1"></input>'+
@@ -32,6 +32,8 @@ describe('Unit tests', function() {
 		var json = $(
 			'<form>' +
 				'<input type="hidden" name="simpleInputHidden" value="1" class="json-form-array"></input>'+
+				'<input type="hidden" name="nested.simpleInputHidden" value="1" class="json-form-array"></input>'+
+				'<input type="hidden" name="simpleInputHiddenList[0]" value="1" class="json-form-array"></input>'+
 				'<input type="text" name="simpleInputText" value="2" class="json-form-array"></input>'+
 				'<input type="checkbox" name="simpleUncheckedCheckbox" value="3" class="json-form-array"></input>'+
 				'<input type="checkbox" name="simpleCheckedCheckbox" value="4" checked  class="json-form-array"></input>'+
@@ -47,6 +49,8 @@ describe('Unit tests', function() {
 				'</select>'+	
 			'</form>').toJson();
 		expect(json.simpleInputHidden).toEqual(['1']);
+		expect(json.nested.simpleInputHidden).toEqual(['1']);
+		expect(json.simpleInputHiddenList[0]).toEqual(['1']);
 		expect(json.simpleInputText).toEqual(['2']);
 		expect(json.simpleUncheckedCheckbox).toBe(undefined);
 		expect(json.simpleCheckedCheckbox).toEqual(['4']);
@@ -59,11 +63,104 @@ describe('Unit tests', function() {
 	it('check if inputs with the same name are serialized as arrays', function() {
 		var json = $(
 			'<form>' +
-				'<input type="hidden" name="simpleInputHidden" value="1"></input>'+
-				'<input type="hidden" name="simpleInputHidden" value="2"></input>'+
+				'<input type="hidden" name="inputArray" value="1"></input>'+
+				'<input type="hidden" name="inputArray" value="2"></input>'+
+				'<input type="hidden" name="inputArray" value="3"></input>'+
 			'</form>').toJson();
-		expect(json.simpleInputHidden).toEqual(['1','2']);
+		expect(json.inputArray).toEqual(['1','2','3']);
 	});
 
+	it('check if nested attributes are serialized according to the rules', function() {
+		var json = $(
+			'<form>' +
+				'<input type="hidden" name="object.simpleInputHidden" value="1"></input>'+
+				'<input type="text" name="object.simpleInputText" value="2"></input>'+
+				'<input type="checkbox" name="object.simpleUncheckedCheckbox" value="3"></input>'+
+				'<input type="checkbox" name="object.simpleCheckedCheckbox" value="4" checked></input>'+
+				'<input type="radio" name="object.simpleUncheckedRadio" value="5"></input>'+
+				'<input type="radio" name="object.simpleCheckedRadio" value="6" checked></input>'+
+				'<input type="radio" name="object.simpleCheckedRadio" value="7"></input>'+
+				'<input type="radio" name="object.simpleCheckedRadio" value="8"></input>'+
+				'<input type="text" name="object.simpleInputWithEmptyValue" value=""></input>'+
+				'<select name="object.simpleSelectField">' +
+					'<option value="A">A</option>'+
+					'<option value="B">B</option>'+
+					'<option value="C" selected="selected">C</option>'+
+				'</select>'+	
+				'<input type="hidden" name="object.inputArray" value="1"></input>'+
+				'<input type="hidden" name="object.inputArray" value="2"></input>'+
+			'</form>').toJson();
+		expect(json.object.simpleInputHidden).toBe('1');
+		expect(json.object.simpleInputText).toBe('2');
+		expect(json.object.simpleUncheckedCheckbox).toBe(undefined);
+		expect(json.object.simpleCheckedCheckbox).toBe('4');
+		expect(json.object.simpleUncheckedRadio).toBe(undefined);
+		expect(json.object.simpleCheckedRadio).toBe('6');
+		expect(json.object.simpleInputWithEmptyValue).toBe(undefined);
+		expect(json.object.simpleSelectField).toBe('C');
+		expect(json.object.inputArray).toEqual(['1','2']);
+	});
+	
+	it('check if objects lists are serialized according to the rules', function() {
+		var json = $(
+			'<form>' +
+				'<input type="hidden" name="object[0].simpleInputHidden" value="1"></input>'+
+				'<input type="text" name="object[0].simpleInputText" value="2"></input>'+
+				'<input type="checkbox" name="object[0].simpleUncheckedCheckbox" value="3"></input>'+
+				'<input type="checkbox" name="object[0].simpleCheckedCheckbox" value="4" checked></input>'+
+				'<input type="radio" name="object[0].simpleUncheckedRadio" value="5"></input>'+
+				'<input type="radio" name="object[0].simpleCheckedRadio" value="6" checked></input>'+
+				'<input type="radio" name="object[0].simpleCheckedRadio" value="7"></input>'+
+				'<input type="radio" name="object[0].simpleCheckedRadio" value="8"></input>'+
+				'<input type="text" name="object[0].simpleInputWithEmptyValue" value=""></input>'+
+				'<select name="object[0].simpleSelectField">' +
+				'<option value="A">A</option>'+
+				'<option value="B">B</option>'+
+				'<option value="C" selected="selected">C</option>'+
+				'</select>'+	
+				'<input type="hidden" name="object[0].inputArray" value="1"></input>'+
+				'<input type="hidden" name="object[0].inputArray" value="2"></input>'+
+
+				'<input type="hidden" name="object[1].simpleInputHidden" value="1"></input>'+
+				'<input type="text" name="object[1].simpleInputText" value="2"></input>'+
+				'<input type="checkbox" name="object[1].simpleUncheckedCheckbox" value="3"></input>'+
+				'<input type="checkbox" name="object[1].simpleCheckedCheckbox" value="4" checked></input>'+
+				'<input type="radio" name="object[1].simpleUncheckedRadio" value="5"></input>'+
+				'<input type="radio" name="object[1].simpleCheckedRadio" value="6" checked></input>'+
+				'<input type="radio" name="object[1].simpleCheckedRadio" value="7"></input>'+
+				'<input type="radio" name="object[1].simpleCheckedRadio" value="8"></input>'+
+				'<input type="text" name="object[1].simpleInputWithEmptyValue" value=""></input>'+
+				'<select name="object[1].simpleSelectField">' +
+				'<option value="A">A</option>'+
+				'<option value="B">B</option>'+
+				'<option value="C" selected="selected">C</option>'+
+				'</select>'+	
+				'<input type="hidden" name="object[1].inputArray" value="1"></input>'+
+				'<input type="hidden" name="object[1].inputArray" value="2"></input>'+
+				'<input type="hidden" name="object[1].inputArray" value="3"></input>'+
+			'</form>').toJson();
+
+		expect(json.object[0].simpleInputHidden).toBe('1');
+		expect(json.object[0].simpleInputText).toBe('2');
+		expect(json.object[0].simpleUncheckedCheckbox).toBe(undefined);
+		expect(json.object[0].simpleCheckedCheckbox).toBe('4');
+		expect(json.object[0].simpleUncheckedRadio).toBe(undefined);
+		expect(json.object[0].simpleCheckedRadio).toBe('6');
+		expect(json.object[0].simpleInputWithEmptyValue).toBe(undefined);
+		expect(json.object[0].simpleSelectField).toBe('C');
+		expect(json.object[0].inputArray).toEqual(['1','2']);
+
+		expect(json.object[1].simpleInputHidden).toBe('1');
+		expect(json.object[1].simpleInputText).toBe('2');
+		expect(json.object[1].simpleUncheckedCheckbox).toBe(undefined);
+		expect(json.object[1].simpleCheckedCheckbox).toBe('4');
+		expect(json.object[1].simpleUncheckedRadio).toBe(undefined);
+		expect(json.object[1].simpleCheckedRadio).toBe('6');
+		expect(json.object[1].simpleInputWithEmptyValue).toBe(undefined);
+		expect(json.object[1].simpleSelectField).toBe('C');
+		expect(json.object[1].inputArray).toEqual(['1','2','3']);
+		
+	});
+	
 	
 });
